3902c4f45b4a5fcf263e61e512a90b692db8e2b9
CDaut <git@cdaut.de>
2024-05-14 08:41:13 +0200

    dev setup and cleanup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake for website development with jekyll";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64bit AMD/Intel x86
+      ];
+
+      forAllSystems = fn:
+        nixpkgs.lib.genAttrs allSystems
+          (system: fn { pkgs = import nixpkgs { inherit system; }; });
+
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "jekyll dev shell";
+          nativeBuildInputs = with pkgs ;[
+            jekyll
+          ];
+        };
+      });
+    };
+}
